2cb9223b5341e50a1991a6c4ae6c317dc690c762
Maël Nison <nison.mael@gmail.com>
2012-06-12 18:00:56 +0200

    Adds user registration

--- src/Woda/UserBundle/Controller/ProfileController.php
+++ src/Woda/UserBundle/Controller/ProfileController.php
@@ -3,11 +3,15 @@
 namespace Woda\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentification\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\SecurityContext;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Woda\UserBundle\Form\UserType;
+use Woda\UserBundle\Entity\User;
 
 class ProfileController extends Controller
 {
@@ -19,6 +23,32 @@ class ProfileController extends Controller
     {
         return array(
             'user' => $this->get('security.context')->getToken()->getUser(),
+        );
+    }
+
+    /**
+     * @Route("/register", name="WodaUserBundle.Profile.register")
+     * @Template("WodaUserBundle:Profile:register.html.twig")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(new UserType(), $user);
+
+        if ($request->getMethod() === 'POST') {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+                $user->setPassword($encoder->encodePassword($user->getPassword(), $user->getSalt()));
+                $entityManager = $this->container->get('doctrine')->getEntityManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                return $this->redirect($this->generateUrl('WodaUserBundle.Security.login'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
         );
     }
 }
