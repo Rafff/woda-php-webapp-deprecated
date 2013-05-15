4f8e99d2909546afc9ae7ee16f8d8879d0d522c8
Maël Nison <nison.mael@gmail.com>
2013-05-15 23:33:33 +0200

    Adds 'real' search

--- src/Woda/SearchBundle/Controller/SearchController.php
+++ src/Woda/SearchBundle/Controller/SearchController.php
@@ -19,60 +19,44 @@ use Woda\SearchBundle\Form\SearchType;
 class SearchController extends Controller
 {
     /**
-     * @Route("/result/{type}/{length}/{offset}", name="WodaSearchBundle.Search.result", defaults={ "length" : 50, "offset" : 0, "type" : "file" }, requirements={ "type" : "file|user"})
+     * @Route("/search", name="WodaSearchBundle.Search.result")
      * @Template("WodaSearchBundle:Search:result.html.twig")
      */
-    public function resultAction($type, $length, $offset)
+    public function searchAction(Request $request)
     {
-        $request = $this->getRequest();
-        if (!$request->isXmlHttpRequest() && !($request->getMethod() === 'POST')) {
-            throw new \Exception('TODO');
-        }
-
         $result = new \stdClass();
         $result->data = array();
         $query = trim($request->get('query'));
 
-        if ($type == "file") {
-            $results = $this->get('doctrine')->getRepository('WodaFSBundle:XFile')->findFileLikeName($query, array(), array($offset, $length));
+        $fileRows = $this->get('doctrine')->getRepository('WodaFSBundle:XFile')
+            ->findFileLikeName($query, array(), array(0, 50));
 
-            if (!empty($results)) {
-                foreach ($results as $r) {
-                    $result->data[] = array(
-                        'name' => $r->getName()
-                    );
-                }
-            }
-
-        } else {
-            $results = $this->get('doctrine')->getRepository('WodaUserBundle:User')->findUserLikeLogin($query, array(), array($offset, $length));
-
-            if (!empty($results)) {
-                foreach ($results as $r) {
-                    $result->data[] = array(
-                        'name' => $r->getLogin()
-                    );
-                }
-            }
+        $files = array( );
+        foreach ($fileRows as $row) {
+            $files[] = array(
+                'name' => $row->getName()
+            );
         }
 
-        $result->count = ($type == 'file') ? 6 : 3000;
-        $result->length = intval($length);
-        $result->offset = intval($offset) + intval($length);
+        $userRows = $this->get('doctrine')->getRepository('WodaUserBundle:User')
+            ->findUserLikeLogin($query, array(), array(0, 50));
+
+        $users = array();
+        foreach ($userRows as $row) {
+            $users[] = array(
+                'name' => $row->getLogin()
+            );
+        }
 
         if ($request->isXmlHttpRequest()) {
             return (new Response(json_encode($result), 200, array('Content-Type'=>'application/json')));
         } else {
             return (
                 array(
-                    'type' => $type,
+                    'displayed' => (!count($files) && count($users)) ? 'users' : 'files',
                     'query' => $query,
-                    'fileOffset' => ($type == 'file') ? ($offset) : (0),
-                    'userOffset' => ($type == 'user') ? ($offset) : (0),
-                    'length' => $length,
-                    'data' => $result->data,
-                    'count' => $result->count
-
+                    'files' => $files,
+                    'users' => $users,
                 )
             );
         }
